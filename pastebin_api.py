654c68f53596f435051fe81b67c492a9d0368cf9
snehamalhotraa <sneha3@flemingcollege.ca>
2024-07-08 18:47:55 -0500

    Completed pastebin.py file.

--- pastebin_api.py
+++ pastebin_api.py
@@ -4,10 +4,10 @@ https://pastebin.com/doc_api
 '''
 import requests
 
-PASTEBIN_API_POST_URL = 'https://pastebin.com/api/api_post.php'
-API_DEV_KEY = 'Put your API key here'
+PASTEBIN_API_POST_URL = 'https://pastebin.com/doc_api'
+API_DEV_KEY = 'HNxK4FPDkHZgU93mrTqNkKpPOGrCf6Vm'
 
-def post_new_paste(title, body_text, expiration='N', listed=True):
+def post_new_paste(title, body_text, expiration='N', public=True):
     """Posts a new paste to PasteBin
 
     Args:
@@ -21,4 +21,24 @@ def post_new_paste(title, body_text, expiration='N', listed=True):
     """    
     # TODO: Function body
     # Note: This function will be written as a group 
-    return
+
+    params = {
+        'api_dev_key': API_DEV_KEY,
+        'api_option': 'paste',
+        'api_paste_code': body_text,
+        'api_paste_name': title,
+        'api_paste_expire_date': expiration,
+        'api_paste_private': 0 if public else 1,
+    }
+
+    print('Creating new API on PasteBin.')
+    
+    # Send the POST request
+    response = requests.post(PASTEBIN_API_POST_URL, data=params)
+    
+    if response.status_code == 200:
+        print('Paste created successfully.')
+        return response.text
+    else:
+        print(f'Failed to create paste. Response code: {response.status_code}')
+        return None
